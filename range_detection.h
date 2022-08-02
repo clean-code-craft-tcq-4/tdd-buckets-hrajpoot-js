fbbfe1c9d418391602071dd04081e36c777ad714
Hardev <65027195+hrajpoot-js@users.noreply.github.com>
2022-08-02 14:49:15 +0530

    Update range_detection.h

--- range_detection.h
+++ range_detection.h
@@ -4,3 +4,4 @@ using namespace std;
 map<string, int> detectAndFetchSampleRanges(int chargingSessionSamples[], int numberOfSamples);
 void display_readings(map<string, int> sampleRangeReadings);
 void sortme(int arr[], int size);
+int bit_to_amps_conversion(double bitInput);
