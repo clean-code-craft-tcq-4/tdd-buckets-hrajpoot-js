6b459cfa2350a7c1333400f98782453310b3a02c
Hardev <65027195+hrajpoot-js@users.noreply.github.com>
2022-08-22 16:14:40 +0530

    Update range_detection.cpp

--- range_detection.cpp
+++ range_detection.cpp
@@ -10,6 +10,22 @@ int bit_to_amps_conversion(double bitInput)
     return round( (10 * bitInput) / 4094 );
 }
 
+string high_fidelity_current_sensing(int inputBits)
+{
+    int apmsInput = bit_to_amps_conversion(inputBits);
+    string returnValue = "";
+    if (apmsInput >= 0 and apmsInput <= 10)
+    {
+        returnValue = to_string(apmsInput) + "A";
+    }
+    else
+    {
+        returnValue = "An error case";
+    }
+
+    return returnValue;
+}
+
 map<string, int> detectAndFetchSampleRanges(int chargingSessionSamples[], int numberOfSamples)
 {
     int rangeReading = 1;
